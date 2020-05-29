8ec1e5f213405a07af99a2632d1347005f342097
Bob <bobmarley9717@gmail.com>
2020-05-29 16:03:28 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This is sample Web Application with the combination of React App and Spring Boot for deploying on heroku. 
+The app will work on http://localhost:8080/ with the react configuration as the front end.
---
+++ src/main/resources/static/precache-manifest.2bd53a49274b3eb08070438c9ca96fa5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c879c22ef92dee224e9247b9addaa21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a73373cabe5971171df",
+    "url": "/static/css/main.4b738920.chunk.css"
+  },
+  {
+    "revision": "531b36856d98405b28f4",
+    "url": "/static/js/2.5e0613e2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5e0613e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a73373cabe5971171df",
+    "url": "/static/js/main.7238e531.chunk.js"
+  },
+  {
+    "revision": "0281e3aa3ea9fc02c242",
+    "url": "/static/js/runtime-main.b12ff581.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
